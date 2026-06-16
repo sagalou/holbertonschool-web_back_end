554ef3d8d1ab86ae2ffbad82c0a4881fd97f44d0
sagalou <sagal.louise.haider@gmail.com>
2026-06-16 12:05:49 +0200

    fix 2-measure_runtime file

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -7,6 +7,7 @@ import time
 from typing import List
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
+
 def measure_time(n: int, max_delay: int) -> float:
     """Measure the toal execution time of wait_n(n, max_delay)
     and return the average time per coroutine"""
